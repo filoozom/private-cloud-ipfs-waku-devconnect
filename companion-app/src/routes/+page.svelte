ead52d01ce9e822b0444261df8bf0abecb79e424
Philippe Schommers <philippe@schommers.be>
2023-11-19 01:36:58 +0300

    feat: display registered apps

--- companion-app/src/routes/+page.svelte
+++ companion-app/src/routes/+page.svelte
@@ -2,12 +2,42 @@
 	import QrCode from '$lib/components/qr-code.svelte';
 	import { onMount } from 'svelte';
 
+	const toHex = (array: Uint8Array) => {
+		return Array.from(array)
+			.map((b) => b.toString(16).padStart(2, '0'))
+			.join('');
+	};
+
+	const formatPublicKey = (key: string) => {
+		return key.substring(0, 4) + '..' + key.substring(key.length - 4, key.length);
+	};
+
+	type Registered = {
+		localPublicKey: string;
+		remotePublicKey: string;
+		metadata: {
+			name: string;
+		};
+	};
+
 	let publicKey = '';
+	let registered: Registered[] = [];
 
 	onMount(async () => {
 		const result = await fetch('http://localhost:8080/pairing');
 		const data = await result.json();
 		publicKey = data.publicKey;
+	});
+
+	onMount(async () => {
+		while (true) {
+			try {
+				const result = await fetch('http://localhost:8080/registered');
+				registered = await result.json();
+			} finally {
+				await new Promise((resolve) => setTimeout(resolve, 5000));
+			}
+		}
 	});
 </script>
 
@@ -19,3 +49,13 @@
 		{publicKey}
 	</div>
 {/if}
+
+<ul>
+	{#each registered as peer}
+		<li>
+			{peer.metadata.name}: local {formatPublicKey(peer.localPublicKey)}, remote {formatPublicKey(
+				peer.remotePublicKey
+			)} (<a href="#">Revoke</a>)
+		</li>
+	{/each}
+</ul>
